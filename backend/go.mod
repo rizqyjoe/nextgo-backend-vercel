b514bfa2f5b3aa2f80af40ad9ef8b85f03e4a8ca
Rizqy Joe <rizqyjoe99@gmail.com>
2025-06-14 22:36:54 +0700

    Hapus folder backend dan frontend yang tidak diperlukan

--- backend/go.mod
+++
@@ -1,8 +0,0 @@
-module sparepart-api
-
-go 1.24.4
-
-require (
-	github.com/golang-jwt/jwt/v5 v5.2.2 // indirect
-	github.com/gorilla/mux v1.8.1 // indirect
-)
--- backend/models/sparepart.go
+++
@@ -1,9 +0,0 @@
-package models
-
-type Sparepart struct {
-	ID       int    `json:"id"`
-	Name     string `json:"name"`
-	Stock    int    `json:"stock"`
-	Unit     string `json:"unit"`
-	Category string `json:"category"`
-}
